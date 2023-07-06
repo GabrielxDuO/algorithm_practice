d61ab6bcfe1add361f90750538e816887ffe7307
GabrielxD <gabrielxduo@outlook.com>
2023-07-06 16:38:37 +0800

    update L2-041 solution

--- pintia/gplt_practice/l2/_041.cpp
+++ pintia/gplt_practice/l2/_041.cpp
@@ -1,30 +1,40 @@
 #include <cstdio>
 #include <stack>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
 const int N = 1e3 + 10, INF = 0x3f3f3f3f;
 int n, m, k;
-int a[N], idx;
+queue<int> que;
 stack<int> box;
 
 int main() {
     scanf("%d%d%d", &n, &m, &k);
-    for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
-    while (true) {
+    for (int i = 0; i < n; ++i) {
+        int x; scanf("%d", &x);
+        que.push(x);
+    }
+    while (!box.empty() || !que.empty()) {
         int prev = INF;
-        for (int i = 0; i < k && idx < n; ++i) {
-            while ((box.empty() || box.top() > prev) && box.size() < m && idx < n) {
-                box.push(a[idx++]);
-            }
-            if (box.top() > prev || box.empty()) break;
-            if (prev != INF) printf(" ");
-            printf("%d", box.top());
-            prev = box.top();
-            box.pop();
+        for (int i = 0; i < k; ++i) {
+            if (!box.empty() && box.top() <= prev) {
+                if (i != 0) printf(" ");
+                prev = box.top();
+                printf("%d", box.top());
+                box.pop();
+            } else if (!que.empty() && que.front() <= prev) {
+                if (i != 0) printf(" ");
+                prev = que.front();
+                printf("%d", que.front());
+                que.pop();
+            } else if (!que.empty() && box.size() < m) {
+                box.push(que.front());
+                que.pop();
+                --i;
+            } else break;
         }
-        if (idx >= n) break;
         puts("");
     }
 
